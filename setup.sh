4dc1e0c169701bcad6ab916d492877191672f8c5
j-wanner <57173138+j-wanner@users.noreply.github.com>
2020-11-07 01:08:28 +0900

    a

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-
-#==================================
-#===== root
-#==================================
-
 #=== mirror config
 #==================================
 #sed -i -e '/^Method.*/d' /etc/pacman-mirrors.conf;sed -i -e '/^OnlyCountry.*/d' /etc/pacman-mirrors.conf;
@@ -21,15 +16,7 @@ sudo pacman -S git --noconfirm
 sudo pacman -S fcitx fcitx-configtool fcitx-mozc fcitx-qt5 fcitx-gtk3 otf-ipaexfont --noconfirm
 rm /etc/fonts/conf.d/70-yes-bitmaps.conf; ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/; fc-cache -fv;
 
+#=== files
 #==================================
-#===== user
-#==================================
-exit;
-
-#=== x
-#==================================
-cd ~; git clone https://github.com/j-wanner/dots.git;
-cp dots/file/fonts/HackGen /usr/share/fonts/;
-echo -e "export LANG=\"ja_JP.UTF-8\"\nexport XMODIFIERS=\"@im=fcitx\"\nexport XMODIFIER=\"@im=fcitx\"\nexport GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport DefaultIMModule=fcitx" > ~/.xprofile
-echo -e "\nexport GTK_IM_MODULE=fcitx\nexport XMODIFIERS=@im=fcitx\nexport QT_IM_MODULE=fcitx" >> ~/.bashrc
-#echo -e "\n\nexec --no-startup-id fcitx" >> ~/.i3/config
+cd /root; git clone https://github.com/j-wanner/setup.git;
+cp -r HackGen /usr/share/fonts/;
