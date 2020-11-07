fede82b6f9f3d08bb7ecc70483e041e51dbe5fa9
j-wanner <57173138+j-wanner@users.noreply.github.com>
2020-11-07 19:21:33 +0900

    a

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 #=== mirror config
 #==================================
 #sed -i -e '/^Method.*/d' /etc/pacman-mirrors.conf; sed -i -e '/^OnlyCountry.*/d' /etc/pacman-mirrors.conf;echo -e \\nMethod = rank\\nOnlyCountry = Japan >> /etc/pacman-mirrors.conf
-pacman-mirrors --country Japan,Taiwan,Singapore,Indonesia --fasttrack 8 && pacman -Syy
+pacman-mirrors --country Japan,Taiwan,Singapore,Indonesia --fasttrack 8 && pacman -Syyu
 
 #=== locale
 #==================================
@@ -12,6 +12,6 @@ sed -i -e 's/^.*LANG.*$/LANG=ja_JP.UTF-8/' /etc/locale.conf; source /etc/locale.
 
 #=== packs    dbus...?
 #==================================
-pacman -S yay git --noconfirm && yay -Syu
+pacman -S yay git --noconfirm
 pacman -S fcitx fcitx-configtool fcitx-mozc fcitx-qt5 fcitx-gtk3 otf-ipaexfont --noconfirm
 rm /etc/fonts/conf.d/70-yes-bitmaps.conf; ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/; fc-cache -fv
