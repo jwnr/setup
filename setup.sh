d7982b97c90a74cb062afe6c4d05b0ffbd26c062
j-wanner <57173138+j-wanner@users.noreply.github.com>
2020-11-11 03:24:57 +0900

    a

--- setup.sh
+++ setup.sh
@@ -3,8 +3,7 @@
 #=== mirror config
 #==================================
 #sed -i -e '/^Method.*/d' /etc/pacman-mirrors.conf; sed -i -e '/^OnlyCountry.*/d' /etc/pacman-mirrors.conf;echo -e \\nMethod = rank\\nOnlyCountry = Japan >> /etc/pacman-mirrors.conf
-#pacman-mirrors --country Japan,Taiwan,Singapore,Indonesia --fasttrack 8 && pacman -Syyu
-pacman-mirrors -f 8 && pacman -Syyu
+pacman-mirrors --country Japan,Taiwan,India,Singapore && pacman -Syyu
 
 #=== locale
 #==================================
