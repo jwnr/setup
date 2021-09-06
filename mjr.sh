dc5e3495282c5d858fd2a6461ebe452313e212ad
jwnr <57173138+jwnr@users.noreply.github.com>
2021-09-07 03:13:05 +0900

    a

--- mjr.sh
+++ mjr.sh
@@ -16,4 +16,4 @@ sed -i -e 's/^.*LANG.*$/LANG=ja_JP.UTF-8/' /etc/locale.conf; source /etc/locale.
 pacman -S git rxvt-unicode fcitx fcitx-configtool fcitx-mozc fcitx-qt5 fcitx-gtk3 otf-ipaexfont --noconfirm
 rm /etc/fonts/conf.d/70-yes-bitmaps.conf; ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/; fc-cache -fv
 
-echo '===== command succeeded ====='
+echo '===== command succeeded =====\n- optimize mirror priority\n- change locale (ja_JP.UTF-8)\n- install packages (git, rxvt, fcitx)\n- prepare font (install IPA, disable bitmap)'
