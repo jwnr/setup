da3c04e36a5153b26c162ea4eb0cbd9cc10c8240
jwnr <57173138+jwnr@users.noreply.github.com>
2021-09-07 03:22:28 +0900

    a

--- mjr.sh
+++ mjr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo '==== command starting ==========================\n- optimize mirror priority\n- change locale (ja_JP.UTF-8)\n- install packages (git, rxvt, fcitx)\n- prepare font (install IPA, disable bitmap)\n================================================\n'
+
 #=== mirror config
 #==================================
 #sed -i -e '/^Method.*/d' /etc/pacman-mirrors.conf; sed -i -e '/^OnlyCountry.*/d' /etc/pacman-mirrors.conf;echo -e \\nMethod = rank\\nOnlyCountry = Japan >> /etc/pacman-mirrors.conf
@@ -16,4 +18,4 @@ sed -i -e 's/^.*LANG.*$/LANG=ja_JP.UTF-8/' /etc/locale.conf; source /etc/locale.
 pacman -S git rxvt-unicode fcitx fcitx-configtool fcitx-mozc fcitx-qt5 fcitx-gtk3 otf-ipaexfont --noconfirm
 rm /etc/fonts/conf.d/70-yes-bitmaps.conf; ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/; fc-cache -fv
 
-echo '===== command succeeded =====\n- optimize mirror priority\n- change locale (ja_JP.UTF-8)\n- install packages (git, rxvt, fcitx)\n- prepare font (install IPA, disable bitmap)'
+echo '==== command succeeded =========================\n'
