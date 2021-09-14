e7eebeb56e8b37e679e4178dc3f7344eecaa2b29
jwnr <57173138+jwnr@users.noreply.github.com>
2021-09-14 22:41:37 +0900

    a

--- mjr.sh
+++ mjr.sh
@@ -13,9 +13,14 @@ sed -i -e 's/^.*LANG.*$/LANG=ja_JP.UTF-8/' /etc/locale.conf; source /etc/locale.
 
 # ==== packs
 # ==================================
-pacman -S --needed --noconfirm git rxvt-unicode dolphin rofi fcitx fcitx-configtool fcitx-mozc fcitx-qt5 fcitx-gtk3 otf-ipaexfont
+pacman -S --needed --noconfirm git rxvt-unicode vivaldi dolphin rofi fcitx fcitx-configtool fcitx-mozc fcitx-qt5 fcitx-gtk3 otf-ipaexfont
+cd /tmp
+git clone https://aur.archlinux.org/visual-studio-code-bin.git
+cd visual-studio-code-bin/
+makepkg -si
+
 
 echo -e "\n==== succeeded ============================================="
 echo -e " + optimize mirror priority\n + sync package databases & upgrade local packages"
-echo -e " + change locale (ja_JP.UTF-8)\n + install packages (git, rxvt, dolphin, rofi, fcitx)\n + install font font (install IPA)"
+echo -e " + change locale (ja_JP.UTF-8)\n + install packages (git, rxvt, vscode, vivaldi, dolphin, rofi, fcitx)\n + install font font (install IPA)"
 echo -e "============================================================\n"
