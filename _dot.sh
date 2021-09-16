36e20fb0432b2c10b5aa1b10d42be361d7803bd5
jwnr <57173138+jwnr@users.noreply.github.com>
2021-09-16 22:59:17 +0900

    a

--- _dot.sh
+++ _dot.sh
@@ -33,6 +33,7 @@ ln -snf ~/dots/dir/.config/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
 ln -snf /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
 fc-cache -fv
 #== Vivaldi
+mkdir ~/.config/vivaldi
 ln -snf ~/dots/dir/.config/vivaldi/Default ~/.config/vivaldi/
 touch ~/.config/vivaldi/"First Run"
 
