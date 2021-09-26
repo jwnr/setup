95a937b54afb7be66d85dbcae165a2acf8129aac
jwnr <57173138+jwnr@users.noreply.github.com>
2021-09-27 01:40:52 +0900

    a

--- _dot.sh
+++ _dot.sh
@@ -26,13 +26,12 @@ done
 cp ~/.i3/config ~/.i3/config_bak
 ln -snf ~/dots/dir/.i3/config ~/.i3/config
 #== fcitx
-# ※profileはリンクが外れる
 mkdir -p ~/.config/fcitx/conf
 ln -snf ~/dots/dir/.config/fcitx/config ~/.config/fcitx/config
-#ln -snf ~/dots/dir/.config/fcitx/profile ~/.config/fcitx/profile
+#cp ~/dots/dir/.config/fcitx/profile ~/.config/fcitx/profile
 ln -snf ~/dots/dir/.config/fcitx/conf/fcitx-classic-ui.config ~/.config/fcitx/conf/fcitx-classic-ui.config
 mkdir -p ~/.config/mozc
-ln -snf ~/dots/dir/.config/mozc/config1.db ~/.config/mozc/config1.db
+cp ~/dots/dir/.config/mozc/config1.db ~/.config/mozc/config1.db
 
 #== font
 cp -rf ~/dots/files/fonts ~/.local/share/
