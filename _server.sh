37c06d48eeb8db1dcb520c69d2ed24778df0cf6c
jwnr <57173138+jwnr@users.noreply.github.com>
2022-08-24 02:41:25 +0900

    a

--- _server.sh
+++ _server.sh
@@ -5,4 +5,4 @@ echo -e "\n=========================================\n==== command started =====
 #==== prepare ssh
 #==================================
 cd ~/.ssh; curl -kOL -u wanner https://k.wnr.jp/srv.tgz; tar xf srv.tgz; rm -f srv.tgz
-chmod -R 400 .ssh/*
+chmod -R 400 github/*
