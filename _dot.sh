e6d428407e336482a41acb12150aa850b168ee8c
jwnr <57173138+jwnr@users.noreply.github.com>
2022-11-03 22:41:52 +0900

    a

--- _dot.sh
+++ _dot.sh
@@ -56,6 +56,6 @@ mkdir -p ~/.config/pcmanfm/default; cp -f ~/dots/dir/.config/pcmanfm/default/pcm
 echo -e "\n==== command succeeded ========================\n+ prepare ssh\n+ download & deploy dot files"
 echo -e "  ├ .config/\n  │  ├ fcitx/...\n  │  ├ fontconfig/...\n  │  ├ mozc/...\n  │  └ pcmanfm/...\n  ├ .i3/...\n  ├ .local/share/...    font files"
 echo -e "  ├ .Xmodmap            keymap\n  ├ .gitconfig\n  ├ .gitignore\n  ├ .xprofile           for fcitx\n  └ urxvt_run.sh        terminal run script"
-echo -e "\n+ display setting\n  echo ~/dots./dir/.screenlayout/xx.sh > ~/.screenlayout/a.sh"
+echo -e "\n+ display setting\n  cat ~/dots/dir/.screenlayout/xx.sh > ~/.screenlayout/a.sh"
 echo -e "\n  (or make xx.sh by arandr and )ln -snf ~/.screenlayout/xx.sh /etc/lightdm/sl/sl.sh"
 echo -e "\n        please reboot system\n===============================================\n"
