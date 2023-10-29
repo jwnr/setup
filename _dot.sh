641a2d8914bb24eb850eaa2215c0b692f37ad755
jwnr <57173138+jwnr@users.noreply.github.com>
2023-10-29 22:26:41 +0900

    a

--- _dot.sh
+++ _dot.sh
@@ -1,57 +1,79 @@
 #!/bin/bash
 
+# SSH > 記述先が.bashrcで良いかどうか
+
 echo -e "\n=========================================\n==== command started ====================\n=========================================\n"
+echo 'Select OS'
+PS3='Selected OS : '
+select OS in 'EndeavourOS-i3wm' 'Manjaro-i3wm' 'cancel'
 
-#==== prepare ssh
-#==================================
-cd ~; curl -kOL -u wanner https://k.jwnr.net/ssh.tgz; tar xf ssh.tgz; rm -f ssh.tgz
-chmod -R 400 .ssh/*
-
-#==== download & deploy
-#==================================
-git clone git@github.com:jwnr/dots.git
-cd ~/dots
-for f in .??*
 do
-  [[ "$f" == ".git" ]] && continue
-  [[ "$f" == ".gitignore" ]] && continue
-  [[ "$f" == ".DS_Store" ]] && continue
+  #==== get key files & dotfiles
+  #==================================
+  cd ~; curl -kOL -u wanner https://k.jwnr.net/ssh.tgz; tar xf ssh.tgz; rm -f ssh.tgz
+  chmod -R 400 .ssh/*
+  git clone git@github.com:jwnr/dots.git
+
+  #== SSH
+  echo -e "\n#==== my commands ====\nexport TERM=xterm-256color" >> ~/.bashrc
+  rm -f ~/.ssh/*; ln -snf ~/dots/dir/.ssh/config ~/.ssh/config
+  chmod 400 ~/dots/dir/.ssh/*/*
+  #== my font
+  cp -rf ~/dots/files/fonts ~/.local/share/
+  mkdir -p ~/.config/fontconfig; ln -snf ~/dots/dir/.config/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
+  fc-cache -fv
+  #== GitHub
+  echo -e "[user]\n  email = 187tch@gmail.com\n    name = wanner" >> ~/.bashrc
+  #== i3wm
+  cp -f ~/.i3/config ~/.i3/config_bak; ln -snf ~/dots/dir/.i3/config ~/.i3/config
+
+
+  if [ $REPLY -eq 1 ]; then
+    #== fcitx設定
+    echo -e "export LANG='ja_JP.UTF-8'\nexport XMODIFIERS='@im=fcitx'\nexport XMODIFIER='@im=fcitx'\n" >> ~/.bashrc
+    echo -e "export GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport DefaultIMModule=fcitx\n" >> ~/.bashrc
+    mkdir -p ~/.config/fcitx/conf; cp -f ~/dots/dir/.config/fcitx/config ~/.config/fcitx/; cp -f ~/dots/dir/.config/fcitx/profile ~/.config/fcitx/
+    ln -snf ~/dots/dir/.config/fcitx/conf/fcitx-classic-ui.config ~/.config/fcitx/conf/fcitx-classic-ui.config
+    mkdir -p ~/.config/mozc; cp -f ~/dots/dir/.config/mozc/config1.db ~/.config/mozc/
+
+    break
+
+  elif [ $REPLY -eq 2]; then
+
+    #== キーマップ
+    ln -snf ~/dots/.Xmodmap ~/
+    #== sway
+    #cp -f ~/.sway/config ~/.sway/config_bak; ln -snf ~/dots/dir/.sway/config ~/.sway/config
+    #== fcitx設定
+    echo -e "export LANG='ja_JP.UTF-8'\nexport XMODIFIERS='@im=fcitx'\nexport XMODIFIER='@im=fcitx'\n" >> ~/.bashrc
+    echo -e "export GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport DefaultIMModule=fcitx\n" >> ~/.bashrc
+    mkdir -p ~/.config/fcitx/conf; cp -f ~/dots/dir/.config/fcitx/config ~/.config/fcitx/; cp -f ~/dots/dir/.config/fcitx/profile ~/.config/fcitx/
+    ln -snf ~/dots/dir/.config/fcitx/conf/fcitx-classic-ui.config ~/.config/fcitx/conf/fcitx-classic-ui.config
+    mkdir -p ~/.config/mozc; cp -f ~/dots/dir/.config/mozc/config1.db ~/.config/mozc/
+    #== fcitxスキン
+    cp -f ~/dots/files/fcitx/default/* /usr/share/fcitx/skin/default/
+    cp -f ~/dots/files/fcitx/icon/* /usr/share/fcitx/mozc/icon/
+    #== default browser
+    sed -i -e 's/userapp-Pale Moon/microsoft-edge/' ~/.config/mimeapps.list
+    #== display
+    mkdir -p ~/.screenlayout; touch ~/.screenlayout/a.sh; chmod 755 ~/.screenlayout/a.sh
+    ln -snf ~/.screenlayout/a.sh /etc/lightdm/sl/sl.sh
+    #== urxvt
+    #xrdb -remove; xrdb -merge ~/.Xresources  <-  link get away...
+    cp -f ~/dots/urxvt_run.sh ~/; chmod 700 ~/urxvt_run.sh
+    #== nvim
+    #ln -snf ~/dots/dir/.config/nvim/init.vim ~/.config/nvim/init.vim
+    #== Pcmanfm
+    mkdir -p ~/.config/pcmanfm/default; cp -f ~/dots/dir/.config/pcmanfm/default/pcmanfm.conf ~/.config/pcmanfm/default/
+
+    break
+  else
   
-  ln -snf ~/dots/"$f" ~/
+    break
+  fi
 done
 
-#== 全般設定
-echo -e "\n#==== my commands ====\nexport TERM=screen.linux" >> ~/.bashrc
-#== git
-rm -f ~/.ssh/*; ln -snf ~/dots/dir/.ssh/config ~/.ssh/config
-chmod 400 ~/dots/dir/.ssh/*/*
-#== sway
-#cp -f ~/.sway/config ~/.sway/config_bak; ln -snf ~/dots/dir/.sway/config ~/.sway/config
-#== i3wm
-cp -f ~/.i3/config ~/.i3/config_bak; ln -snf ~/dots/dir/.i3/config ~/.i3/config
-#== fcitx設定
-mkdir -p ~/.config/fcitx/conf; cp -f ~/dots/dir/.config/fcitx/config ~/.config/fcitx/; cp -f ~/dots/dir/.config/fcitx/profile ~/.config/fcitx/
-ln -snf ~/dots/dir/.config/fcitx/conf/fcitx-classic-ui.config ~/.config/fcitx/conf/fcitx-classic-ui.config
-mkdir -p ~/.config/mozc; cp -f ~/dots/dir/.config/mozc/config1.db ~/.config/mozc/
-#== fcitxスキン
-cp -f ~/dots/files/fcitx/default/* /usr/share/fcitx/skin/default/
-cp -f ~/dots/files/fcitx/icon/* /usr/share/fcitx/mozc/icon/
-#== font
-cp -rf ~/dots/files/fonts ~/.local/share/
-mkdir -p ~/.config/fontconfig; ln -snf ~/dots/dir/.config/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
-fc-cache -fv
-#== default browser
-sed -i -e 's/userapp-Pale Moon/microsoft-edge/' ~/.config/mimeapps.list
-#== display
-mkdir -p ~/.screenlayout; touch ~/.screenlayout/a.sh; chmod 755 ~/.screenlayout/a.sh
-ln -snf ~/.screenlayout/a.sh /etc/lightdm/sl/sl.sh
-#== urxvt
-#xrdb -remove; xrdb -merge ~/.Xresources  <-  link get away...
-cp -f ~/dots/urxvt_run.sh ~/; chmod 700 ~/urxvt_run.sh
-#== nvim
-#ln -snf ~/dots/dir/.config/nvim/init.vim ~/.config/nvim/init.vim
-#== Pcmanfm
-mkdir -p ~/.config/pcmanfm/default; cp -f ~/dots/dir/.config/pcmanfm/default/pcmanfm.conf ~/.config/pcmanfm/default/
+
 
 echo -e "\n==== command succeeded ========================\n+ prepare ssh\n+ download & deploy dot files"
 echo -e "  ├ .config/\n  │  ├ fcitx/...\n  │  ├ fontconfig/...\n  │  ├ mozc/...\n  │  └ pcmanfm/...\n  ├ .i3/...\n  ├ .local/share/...    font files"
