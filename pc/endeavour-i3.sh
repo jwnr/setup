b6e46bb47d6d6f2151b0408c7fe60d79dc54d9f9
jwnr <57173138+jwnr@users.noreply.github.com>
2023-12-11 08:10:09 +0900

    a

--- pc/endeavour-i3.sh
+++ pc/endeavour-i3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "\n============================================================\n==== starting ==============================================\n============================================================"
-echo -e '*****************************************************'
+echo -e '\n*****************************************************'
 echo -e '  + You need to do "sudo xx" before this shell.\n        Have you done it?\n  + This shell cannot pipe to "| sh".\n        Download and execute, ok?.'
 echo -e '*****************************************************\n'
 
@@ -28,7 +28,7 @@ echo $pswd | sudo -S sh -c 'eos-rankmirrors --sort age && eos-update --yay'
 # ==================================
 echo $pswd | sudo -S sed -i -e 's/^.*LANG.*$/LANG=ja_JP.UTF-8/' /etc/locale.conf; source /etc/locale.conf
 
-# ==== packs
+# ==== packages
 # ==================================
 # neovim jq nodejs-lts nodejs-lts-gallium bun deno(deno upgrade)
 # rxvt-unicode dolphin rofi webp-pixbuf-loader flameshot
@@ -51,7 +51,7 @@ cp -rf ~/dots/files/fonts ~/.local/share/
 mkdir -p ~/.config/fontconfig; ln -snf ~/dots/dir/.config/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
 fc-cache -fv
 
-# ==== fcitx設定
+# ==== fcitx
 # ==================================
 echo $pswd | sudo -S sh -c 'echo -e "export XMODIFIERS=@im=fcitx\nexport GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\n" >> /etc/profile'
 rsync -a ~/dots/end/dir/.config/fcitx5/* ~/.config/fcitx5/
@@ -60,35 +60,32 @@ rsync -a ~/dots/end/dir/.config/mozc/* ~/.config/mozc/
 # ==== display
 # ==================================
 
-
 # == i3wm
+# ==================================
 cp -f ~/.config/i3/config ~/.config/i3/config_bak; ln -snf ~/dots/end/dir/.config/i3/config ~/.config/i3/config
-
-
 
 # ==== other setting
 # ==================================
-# == hosts customize for Edge
+# hosts customize for Edge
 echo $pswd | sudo -S sh -c 'echo -e \\n127.0.0.1 browser.events.data.msn.com\\n127.0.0.1 c.msn.com\\n127.0.0.1 sb.scorecardresearch.com\\n127.0.0.1 api.msn.com\\n >> /etc/hosts'
-# == GitHub
+# GitHub
 echo -e "[user]\n  email = 187tch@gmail.com\n  name  = wanner" >> ~/.gitconfig
-# == terminal
+# terminal
 echo -e "\n#==== my commands ====\nexport TERM=xterm-256color" >> ~/.bashrc
-
-
 
 
 echo -e "\n==== succeeded ============================================="
 echo -e " + optimize mirror priority\n + sync package databases & upgrade local packages"
 echo -e " + change locale (ja_JP.UTF-8)"
 echo -e " + prepare font (install IPAex, disable bitmap font)"
-echo -e " + remove package\n    - ClipIt"
+echo -e " + remove package\n    - nothing"
 echo -e " + install packages (by pacman)\n    - git, rxvt, unzip, unrar, Vivaldi, dolphin, rofi, fcitx, flameshot"
 echo -e "    - Node.js(latest), npm, Deno(latest)"
-echo -e " + install packages (by pamac)\n    - Chrome, Chrome beta, Edge, VS Code"
+echo -e " + install packages (by yay)\n    - Chrome, Chrome beta, Edge, VS Code"
 echo -e " + block some URL by adding hosts (for Edge)"
 echo -e " +--------------------+------------------+"
 echo -e " | screen recorder    | Vokoscreen       |"
 echo -e " | video editor       | Shotcut          |"
 echo -e " | Markdown editor    | Obsidian         |"
+echo -e " +--------------------+------------------+"
 echo -e "============================================================\n"
