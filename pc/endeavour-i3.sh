cb07d27d5e022e8efc8084ac8ad1a3d5edd5672e
jwnr <57173138+jwnr@users.noreply.github.com>
2023-12-13 13:55:20 +0900

    a

--- pc/endeavour-i3.sh
+++ pc/endeavour-i3.sh
@@ -65,7 +65,11 @@ fc-cache -fv
 
 # ==== fcitx
 # ==================================
+# /usr/share/icons/hicolor/00x00/apps/
+# 32 48 128
 echo $pswd | sudo -S sh -c 'echo -e "export XMODIFIERS=@im=fcitx\nexport GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\n" >> /etc/profile'
+cp ~/dots/files/fcix5/icon/* /tmp/zxcv/
+echo $pswd | sudo -S sh -c 'cp -f /tmp/zxcv/* /usr/share/icons/hicolor/32x32/apps/'
 rsync -a ~/dots/end/dir/.config/fcitx5/* ~/.config/fcitx5/
 rsync -a ~/dots/end/dir/.config/mozc/* ~/.config/mozc/
 
